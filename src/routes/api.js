6f5d20daa72638724004e2be09c9eacb6714a25e
Shatabdi Barua <u1404023@student.cuet.ac.bd>
2023-11-22 22:15:09 +0600

    completed comment crud operations without user authentication

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const router= express.Router()
-
+const express = require("express");
+const router = express.Router();
+const {
+  createComment,
+  getComments,
+  updateComment,
+  deleteComment,
+} = require("../controllers/CommentController");
 // API Routing
+router.post("/comment", createComment);
+router.get("/comments", getComments);
+router.put("/comment/:commentId", updateComment);
+router.delete("/comment/:commentId", deleteComment);
 
-
-
-
-module.exports=router
+module.exports = router;
